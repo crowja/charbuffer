0fc64b5597d60883bc4ece0fc45861eea234e204
jac <crowja@gmail.com>
2021-12-16 20:04:52 -0600

    .

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # charbuffer 0.0.0.
 
+A pretty limited-purpose C scrap, but handy in the right places.
+
+```c
+z = charbuf_new()      /* return a charbuf */
+charbuf_push(z, 'a')    /* push a char onto the buffer */
+charbuf_expose(z)       /* returns the pointer to the internal buffer */
+charbuf_reset(z)        /* restart/reuse the charbuff */
+charbuf_free(&z)        /* free memory associated with z */
+```
+
+`charbuf_expose()` lets you access what you've accumulated. The address it
+returns may change in subsequent calls to `charbuf_push()`. It's not 
+a null-terminated string.
--- t/test.c
+++ t/test.c
@@ -19,6 +19,42 @@ test_constr(void)
    ASSERT_EQUALS(NULL, z);
 }
 
+static void
+test01(void)
+{
+   struct charbuf *z;
+   unsigned  i, niter = 20000;
+
+   fprintf_test_info(stdout, "test01", "The whole damn lot of them ...");
+   z = charbuf_new();
+   ASSERT("test01", z);
+
+   ASSERT_EQUALS(0, charbuf_len(z));
+
+   for (i = 0; i < niter; i++) {
+      charbuf_push(z, 'a');
+      charbuf_push(z, 'A');
+      charbuf_push(z, 'B');
+      ASSERT_EQUALS(3 + 3 * i, charbuf_len(z));
+   }
+   ASSERT_EQUALS(3 * niter, charbuf_len(z));
+
+   for (i = 0; i < niter;) {
+      ASSERT_EQUALS('a', (charbuf_expose(z))[i]);
+      i++;
+      ASSERT_EQUALS('A', (charbuf_expose(z))[i]);
+      i++;
+      ASSERT_EQUALS('B', (charbuf_expose(z))[i]);
+      i++;
+   }
+
+   charbuf_reset(z);
+   ASSERT_EQUALS(0, charbuf_len(z));
+
+   charbuf_free(&z);
+   ASSERT_EQUALS(NULL, z);
+}
+
 #if 0                                            /* 12 yy */
 static void
 test_stub(void)
@@ -40,6 +76,7 @@ main(void)
    printf("%s\n", charbuf_version());
 
    RUN(test_constr);
+   RUN(test01);
 
    return TEST_REPORT();
 }
